4bc0bc90641a01cec791533a66d28d27ae582825
Pamela Karina Canale <pamela.karina@hotmail.com>
2024-02-25 19:49:43 -0300

    003 - Criado o content

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,27 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title":"Aspectos de liderança presentes no filme Moana – Um Mar de Aventuras",
+    "description":"Se você não conhece “Moana, um mar de aventuras”, é uma animação dos estúdios Walt Disney Pictures, totalmente diferente do que estamos acostumados a ver quando se trata de um filme da companhia.",
+    "photoCover":"http://www.petadm.ufc.br/wp-content/uploads/2020/10/MOANA-2.jpg"
+  },
+  {
+    "id":"2",
+
+    "title":"Por que hoje é comemorado o Dia do Stitch?",
+    "description":"Descrição aqui",
+    "photoCover":"https://recreio.uol.com.br/media/_versions/disney/dia_do_stitch_capa_widelg.jpg"
+  },
+  {
+    "id":"3",
+    "title":"Má notícia! Disney vai aumentar preços de ingressos",
+    "description":"Descrição aqui",
+    "photoCover":"https://www.melhoresdestinos.com.br/wp-content/uploads/2022/11/disney-aumenta-precos-ingressos-orlando-1-1536x805.jpeg"
+  },
+  {
+    "id":"4",
+    "title":"Disney está desenvolvendo live-action de Rapunzel",
+    "description":"Descrição aqui",
+    "photoCover":"https://assets.papelpop.com/wp-content/uploads/2020/02/0_5DEcZJ__cH3ansKS.jpg"
+  }
+]
